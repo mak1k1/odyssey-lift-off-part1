b1fc63c94b131b682d83f3d7e4cf773bb797ffd7
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-05 23:31:25 +0200

    Completed fifth part of the course

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react"
+import ReactDOM from "react-dom"
+import GlobalStyles from "./styles"
+import Pages from "./pages"
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "https://odyssey-makiki.herokuapp.com/",
   cache: new InMemoryCache(),
-
 })
-
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
